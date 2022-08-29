b1c4b52ed8fadbcbb7416bbd579ba772078596ef
Lucas Calabria <lucascalabria86@gmail.com>
2022-08-29 13:27:31 -0300

    Commitando exercício 2

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+let perguntaComida = "Você já almoçou hoje ?"
+let respostaComida = prompt(perguntaComida);
+
+let perguntaBanho = "Você já tomou banho hoje ?"
+let respostaBanho = prompt(perguntaBanho);
+
+let perguntaEstudo = "Você já estudou hoje ?"
+let respostaEstudo = prompt(perguntaEstudo);
+
+console.log((perguntaComida), (respostaComida));
+console.log((perguntaBanho), (respostaBanho));
+console.log((perguntaEstudo), (respostaEstudo));
+
+
